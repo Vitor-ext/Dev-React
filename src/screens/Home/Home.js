cf6ba374cdce594e947268bb7e5715063bcefafb
VITOR SANTANA DE JESUS <sn1079379@cfp127aluno.local>
2022-12-20 11:48:38 -0300

    DEV React

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,19 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  Image,
+  Alert
+} from 'react-native';
 import styles from "./styles";
-import { useCallback } from 'react';
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import Api from '../../api';
+import SignInput from '../../components/SignInput';
+import Aviao from '../../../src/assets/Aviao.png';
+
 
 export default function App() {
 
   const navigation = useNavigation();
 
+  const [valor1Field, setvalor1Field] = useState('1,00');
+  const [valor2Field, setvalor2Field] = useState('1,00');
+
+  const Aposta1 = (req) => {
+    Api.post("Definir URL do BACK", {
+      //Analisar after
+    })
+      .then((response) => {
+        console.log(response.data);
+        // Analisar o que faremos após coletar o resultado
+      })
+      .catch((e) => {
+        Alert.alert('Erro', 'Impossivel apostar nesta rodada')
+        console.log(e);
+      });
+  }
+
+  const Aposta2 = (req) => {
+    Api.post("Definir URL do BACK", {
+      //Analisar after
+    })
+      .then((response) => {
+        console.log(response.data);
+        // Analisar o que faremos após coletar o resultado
+      })
+      .catch((e) => {
+        Alert.alert('Erro', 'Impossivel apostar nesta rodada')
+        console.log(e);
+      });
+  }
+
 
   return (
-    <View styles={styles.container}>
-      <Text>Vamos Jogar Xadrez!</Text>
-      < Image source={tab_4} style={{width: 2000, height: 300}} resizeMode="contain"/>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <View >
+        <Text style={styles.Text}>Faça suas apostas!</Text>
+        <StatusBar style="auto" />
+      </View>
+
+      <View >
+        <Image source = {Aviao} style = {styles.Image}/>
+        <StatusBar style="auto" />
+      </View>
+
+      <View style={styles.InputArea} >
+
+        <SignInput
+          placeholder="0,00"
+          value={valor1Field}
+          onChangeText={t => setvalor1Field(t)}
+        />
+      </View>
+
+      <TouchableOpacity
+        style={styles.btaposta}
+        onPress={Aposta1}
+      >
+        <Text>APOSTAR</Text>
+      </TouchableOpacity>
+
+      <View style={styles.InputArea} >
+
+        <SignInput
+          placeholder="0,00"
+          value={valor2Field}
+          onChangeText={t => setvalor2Field(t)}
+        />
+      </View>
+
+      <TouchableOpacity
+        style={styles.btaposta}
+        onPress={Aposta2}
+      >
+        <Text>APOSTAR</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -3,10 +3,36 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#CB90F3',
       alignItems: 'center',
       justifyContent: 'center',
     }, 
+      Text:{
+        margin:10,
+        fontSize: 35,
+        color:"#800080",
+        padding: 20,
+      },
+      Image:{
+        alignItems: 'center',
+        width: 300,
+        height: 200,
+        padding: 20,
+        margin:10,
+      },
+      btaposta:{
+        alignItems: "center",
+        padding: 15,
+        backgroundColor: "#800080",
+        color: "#000000",
+        borderRadius: 10,
+        width: 250,
+        height: 50,
+        margin: 5,
+      },
+      InputArea:{
+        padding: 15,
+      }
   });
 
 export default styles;
